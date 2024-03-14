06417ba5b0942afd3fd2949e044dd46c93f7e516
Absozero <aswsub123@gmail.com>
2024-03-14 07:57:16 -0700

    feat(Plugins Added): Added nvim-tree.lua to Nvim configuration

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,6 +1,17 @@
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     { "navarasu/onedark.nvim" },
+    {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+    	"nvim-tree/nvim-web-devicons",
+    	},
+  	config = function()
+    	   require("nvim-tree").setup {
+    },
+
     {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
     { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
